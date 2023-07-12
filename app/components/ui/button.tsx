29976ff0479857e8d4df859ca50955e644db8c79
ermiyas <128813243+ermi1999@users.noreply.github.com>
2023-07-12 12:42:19 +0300

    final

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,22 +1,37 @@
-import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
+
+import { cn } from "@/lib/utils";
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
     return (
       <button
+        type={type}
         className={cn(
           `
-    w-auto rounded-full bg-black border-transparent
-    px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50
-    text-white font-semibold
-    hover:opacity-75
-    transition
-    `,
+        w-auto 
+        rounded-full 
+        bg-black
+        border
+        border-transparent
+        px-5 
+        py-3 
+        disabled:cursor-not-allowed 
+        disabled:opacity-50
+        text-white
+        font-semibold
+        hover:opacity-75
+        transition
+      `,
+          disabled && "opacity-75 cursor-not-allowed",
           className
         )}
+        disabled={disabled}
         ref={ref}
+        {...props}
       >
         {children}
       </button>
@@ -24,6 +39,6 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   }
 );
 
-Button.displayName = "Botton";
+Button.displayName = "Button";
 
 export default Button;
--- app/components/ui/product-card.tsx
+++ app/components/ui/product-card.tsx
@@ -4,14 +4,39 @@ import Image from "next/image";
 import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
+import { useRouter } from "next/navigation";
+import usePreViewModal from "@/hooks/use-preview-modal";
+import { MouseEventHandler } from "react";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const cart = useCart();
+  const previewModal = usePreViewModal();
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
+  };
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
+  };
+
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       {/* images and actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
@@ -23,11 +48,11 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
